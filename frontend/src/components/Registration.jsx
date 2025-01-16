c151dc0750cfa015109f9f81e331552172987269
MeisZy <zyrusnw@gmail.com>
2025-01-16 23:01:01 +0800

    update: registration form in progress.

--- frontend/src/components/Registration.jsx
+++ frontend/src/components/Registration.jsx
@@ -9,9 +9,42 @@ function Registration() {
           <a>Collectius</a>
         </div>
     </nav>
-    <form className='registrationform'>
-        test
-    </form>
+    <form className="registrationform">
+    <h4>Registration</h4>
+    <div className="formcaption">
+        <p>Enter your details to register</p>
+        <p>Profile Information</p>
+    </div>
+    <div className="formrow">
+        <input type="text" placeholder="First Name" />
+        <input type="text" placeholder="Middle Name" />
+        <input type="text" placeholder="Last Name" />
+    </div>
+    <div className="formrow">
+        <input type="text" placeholder="Birth Date" />
+        <select id="gender" placeholder='gender'>
+          
+        </select>
+    </div>
+    <p>Address</p>
+    <div className="formrow">
+        <input type="text" placeholder="Street Address" />
+    </div>
+    <div className="formrow">
+        <input type="text" placeholder="City" />
+        <input type="text" placeholder="State / Province" />
+        <input type="text" placeholder="Postal Code" />
+    </div>
+    <div className="formrow">
+        <input type="email" placeholder="Email" />
+        <input type="text" placeholder="Phone Number" />
+    </div>
+    <div className="formrow">
+        <input type="password" placeholder="Password" />
+        <input type="password" placeholder="Confirm Password" />
+    </div>
+    <button type="submit" className="btn">Register</button>
+</form>
     </>
   )
 }
