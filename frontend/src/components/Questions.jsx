5096744d5d83385db5623e096fca9be21971af6a
MeisZy <zyrusnw@gmail.com>
2025-04-02 15:51:25 +0800

    update: lastest

--- frontend/src/components/Questions.jsx
+++ frontend/src/components/Questions.jsx
@@ -14,18 +14,12 @@ function Questions() {
     <>
         <nav>
             <a href="#" onClick={handleReturn}>Back</a>
-            <p>I forgot my password. How can I reset it?
-            Click the “Forgot Password” link on the login page, enter your email, and follow the instructions to reset your password</p>
-            <p>The system is not letting me upload my resume. What should I do?
-Ensure your resume is in the required format (e.g., PDF, DOCX)
-Check the file size limit
-Try using a different browser or device
-If the issue persists, contact the company’s HR support</p>
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
         </nav>
+        <div className='proper'>
+          <div className='instance'>
+            <p>test</p>
+          </div>
+        </div>
     </>
   )
 }
